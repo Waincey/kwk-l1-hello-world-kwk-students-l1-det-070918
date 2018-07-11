df75e49b7f40729b95380a9fb727b9a91d60f11a
Waincey <waincey.chan@gmail.com>
2018-07-11 14:40:31 +0000

    automatically backed up by learn

--- time_of_day.rb
+++ time_of_day.rb
@@ -1,16 +1,12 @@
 def time_of_day(time) 
-  
 if time >= 7 && < 11
   puts "Good Morning!"
-  
 elsif time >= 11 && time < 17
   puts "Good Afternoon!"
-  
   elsif time >= 17 && 20
   puts "Good Evening!"
 else
   puts "Good Night!"
 end
 end
-
 time_of_day(18)
